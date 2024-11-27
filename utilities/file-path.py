0ba989c4bb4026527c80ce89fc58dfed9bd58cc0
David Hartman <dhartman@it2solutions.com>
2024-11-26 22:32:16 -0500

    Added new scripts

--- utilities/file-path.py
+++ utilities/file-path.py
@@ -18,7 +18,7 @@ from pprint import pp
 # use suffixes if more than one suffix (e.g. .tar.gz) NOTE: returns an array
 all_suffixes = Path('my/library.tar.gz').suffixes
 # all_suffixes = ['.tar', '.gz']
-filter_suffixes = [".py", ".md"]
+filter_suffixes_default = [".py", ".md"]
 
 # to create a single string with all extensions
 # file_type_extension = join(all_suffixes.suffixes)
@@ -30,10 +30,11 @@ all_suffixes_no_file = Path('my/library').suffixes
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 
-def recursive_iterdir(path: Path) -> list:
+def recursive_iterdir(path: Path, filter_suffixes=None) -> list:
     """recursively iterates over the provided path location
 
     Args:
+        filter_suffixes(list): An optional argument that contains a list of suffixes to filter
         path(Path): The starting directory path location
 
     Returns:
@@ -41,26 +42,33 @@ def recursive_iterdir(path: Path) -> list:
     """
     items = list()
     for item in path.iterdir():
-        if item.suffix in filter_suffixes:
+        if filter_suffixes is None:
             items.append(item)
+        else:
+            if item.suffix in filter_suffixes:
+                items.append(item)
         if item.is_dir():
-            items.extend(recursive_iterdir(item))
+            items.extend(recursive_iterdir(item, filter_suffixes))
     return items
 
 
-def recursive_iterdir_gen(path: Path):
+def recursive_iterdir_gen(path: Path, filter_suffixes=None):
     """recursively iterates over the provided path location
 
     This function works as an iterative function that can be used in loops
 
     Args:
+        filter_suffixes:
         path(Path): The starting directory path location
     """
     for item in path.iterdir():
-        if item.suffix in filter_suffixes:
+        if filter_suffixes is None:
             yield item
+        else:
+            if item.suffix in filter_suffixes:
+                yield item
         if item.is_dir():
-            yield from recursive_iterdir_gen(item)
+            yield from recursive_iterdir_gen(item, filter_suffixes=filter_suffixes)
 
 
 def main():
@@ -71,8 +79,9 @@ def main():
     # Create a Path object
     starting_dir = Path(args.start_dir)
     #pp(list(starting_dir.iterdir()))
-    for files in recursive_iterdir_gen(starting_dir):
+    for files in recursive_iterdir_gen(starting_dir, filter_suffixes_default):
         print(files)
+    pp(list(recursive_iterdir(starting_dir, filter_suffixes_default)))
 
 if __name__ == "__main__":
     main()
